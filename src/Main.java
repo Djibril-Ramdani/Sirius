8eaa567f1a398208610b87c1c95c65d665612473
Meissa <meissachaoui05@gmail.com>
2025-12-12 15:32:20 +0100

    ggggg

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Caaaa marche pas .Coucou.Je vérifie que tout fonctionne. Test");
+        System.out.printf("Caaaa marche pas fffffff.Coucou.Je vérifie que tout fonctionne. Test");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
